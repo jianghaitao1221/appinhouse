4cb25fde7a9ff2df72d3070662dbbe8b21a06c7a
姜海涛 <jianghaitao@kingsoft.com>
2020-12-10 15:53:03 +0800

    修改copy-conf.sh

--- deploy/copy-conf.sh
+++ deploy/copy-conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APPINHOUSE_HOME=/srv/appinhouse
-REDIS_CONFIG_PATH =/srv/redis/conf
+REDIS_CONFIG_PATH =/srv/appinhouse/redis/conf
 
 sudo cp /tmp/appinhouse/docker-compose.yml $APPINHOUSE_HOME
 sudo cp /tmp/appinhouse/redis.conf $REDIS_CONFIG_PATH
