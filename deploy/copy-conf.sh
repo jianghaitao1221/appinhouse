fde69c096a4433458867f6602fd48f89869c6bfd
姜海涛 <jianghaitao@kingsoft.com>
2020-12-10 16:20:04 +0800

    发布增加app.conf

--- deploy/copy-conf.sh
+++ deploy/copy-conf.sh
@@ -2,6 +2,8 @@
 
 APPINHOUSE_HOME=/srv/appinhouse
 REDIS_CONFIG_PATH=/srv/appinhouse/redis/conf
+APPINHOUSE_CONFIG_PATH=$APPINHOUSE_HOME/conf
 
 sudo cp /tmp/appinhouse/docker-compose.yml $APPINHOUSE_HOME
 sudo cp /tmp/appinhouse/redis.conf $REDIS_CONFIG_PATH
+sudo cp /tmp/appinhouse/app.conf $APPINHOUSE_CONFIG_PATH
