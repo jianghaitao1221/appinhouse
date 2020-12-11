194cde9f41cfd224bdb27bea3b1a1bf2337dbf87
姜海涛 <jianghaitao@kingsoft.com>
2020-12-11 15:23:18 +0800

    修改发布脚本

--- deploy/init.sh
+++ deploy/init.sh
@@ -8,21 +8,19 @@ echo "init appinhouse conf path"
 if [ ! -d "$APPINHOUSE_CONF_PATH" ]; then
     mkdir -p "$APPINHOUSE_CONF_PATH"
     echo "mkdir "$APPINHOUSE_CONF_PATH
-    chmod 765 $APPINHOUSE_CONF_PATH
+    chmod 766 $APPINHOUSE_CONF_PATH
 fi
 
 echo "init redis conf path"
 if [ ! -d "$REDIS_CONF_PATH" ]; then
     mkdir -p "$REDIS_CONF_PATH"
     echo "mkdir "$REDIS_CONF_PATH
-    chmod 765 $REDIS_CONF_PATH
-    chown 1000 $REDIS_CONF_PATH
+    chmod 766 $REDIS_CONF_PATH
 fi
 
 echo "init redis data path"
 if [ ! -d "$REDIS_DATA_PATH" ]; then
     mkdir -p "$REDIS_DATA_PATH"
     echo "mkdir "$REDIS_DATA_PATH
-    chmod 765 $REDIS_DATA_PATH
-    chown 999 $REDIS_DATA_PATH
+    chmod 766 $REDIS_DATA_PATH
 fi
