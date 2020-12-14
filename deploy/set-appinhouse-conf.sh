e8c785079c4a49353870935fa0940d99ad447d7d
姜海涛 <jianghaitao@kingsoft.com>
2020-12-14 17:28:43 +0800

    修改发布流程

--- deploy/set-appinhouse-conf.sh
+++ deploy/set-appinhouse-conf.sh
@@ -7,6 +7,5 @@ if [ ! -n "$1" ] ;then
     exit 1
 fi
 SECRET_KEY=$1
-echo "init redis conf path"
 sed -i '/^secret_key/csecret_key = '$SECRET_KEY $APPINHOUSE_CONF_PATH/app.conf
 sed -i '/^addr/caddr = 'redis:6379 $APPINHOUSE_CONF_PATH/app.conf
